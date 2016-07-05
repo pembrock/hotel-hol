9d15835427296c240b8a62639a992bbe72d3b474
Kirill Pasikuta <pembrock@gmail.com>
2016-07-05 23:00:40 +0300

    fxd. update

--- inc/pages/info.php
+++ inc/pages/info.php
@@ -6,7 +6,9 @@
  * Time: 22:48
  */
 if(isset($_GET['id'])){
-    $info = $fpdo->from('information')->select('id, title_' . $lang['type'] . ' AS title, text_' . $lang['type'] . ' AS text')->where(array('isActive' => 1, 'id' => intval($_GET['id'])))->fetchAll();
+    $info = $fpdo->from('information')->select('id, title_' . $lang['type'] . ' AS title, text_' . $lang['type'] . ' AS text, meta_desc_' . $lang['type'] . ' AS meta_desc, meta_key_' . $lang['type'] . ' AS meta_key')->where(array('isActive' => 1, 'id' => intval($_GET['id'])))->fetchAll();
+    $twig->addGlobal('metadesc', $info[0]['meta_desc']);
+    $twig->addGlobal('metakey', $info[0]['meta_key']);
     echo $twig->render('/front/info-item.html.twig', array('info' => $info));
 }
 else {
--- inc/pages/promo.php
+++ inc/pages/promo.php
@@ -6,7 +6,9 @@
  * Time: 22:48
  */
 if(isset($_GET['id'])){
-    $promo = $fpdo->from('promo')->select(null)->select('id, title_' . $lang['type'] . ' AS title, description_' . $lang['type'] . ' AS description, text_'  . $lang['type'] . ' AS text, start_ts, stop_ts, image, slide_image')->where(array('isActive' => 1, 'id' => intval($_GET['id'])))->fetchAll();
+    $promo = $fpdo->from('promo')->select(null)->select('id, title_' . $lang['type'] . ' AS title, description_' . $lang['type'] . ' AS description, text_'  . $lang['type'] . ' AS text, start_ts, stop_ts, image, slide_image, meta_desc_' . $lang['type'] . ' AS meta_desc, meta_key_' . $lang['type'] . ' AS meta_key')->where(array('isActive' => 1, 'id' => intval($_GET['id'])))->fetchAll();
+    $twig->addGlobal('metadesc', $promo[0]['meta_desc']);
+    $twig->addGlobal('metakey', $promo[0]['meta_key']);
     echo $twig->render('/front/promo-item.html.twig', array('promo' => $promo));
 }
 else {
